29741f186e8ad8109ddefde2d47507c7e5ff76b3
dlusion <dlusion@uw.edu>
2020-03-12 01:28:36 -0700

    viz2?

--- d3_viz2.js
+++ d3_viz2.js
@@ -1,185 +1,241 @@
 'use strict';
-// Instead of monthly price, could do like availability year round?
-(function() {
+let data = ""
+let svg = ""
+let tooltip = ""
 
-    let data = ""
-    let svg = ""
-    let tooltip = ""
+const measurements = {
+    margin: 50,
+    width: 500,
+    height: 500
+}
 
-    const measurements = {
-        margin: 50,
-        width: 500,
-        height: 500
-    }
+window.onload = function() {
+    svg = d3.select("body").append("svg")
+        .attr("width", 700)
+        .attr("height", 700);
 
-    window.onload = function() {
-        svg = d3.select("body").append("svg")
-            .attr("width", 700)
-            .attr("height", 700);
+    console.log("here")
 
-        tooltip = d3.select("body").append("div")
-            .attr("class", "tooltip")
-            .attr('style', 'position: absolute; opacity: 0;');
+    tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .attr('style', 'position: absolute; opacity: 0;');
 
-        d3.csv("data/listings.csv").then(function (data) {
-            makeHistogram(data)
-        })
-    }
-
-    function makeHistogram(data) {
-        let price = data.map((row) => row["monthly_price"]);
-        price = price.filter(Boolean);
-        for (var i = 0; i < price.length; i++) {
-          var number = Number(price[i].replace(/[^0-9\.]+/g,""));
-          price[i] = number;
-        }
-
+    d3.csv("./cal2.csv").then(function (data) {
         data.forEach(function (d) {
-            d["monthly_price"] = d["monthly_price"].replace(/[\$,]/g, '')
-            d["monthly_price"] = +d["monthly_price"]
+            d["price"] = d["price"].replace(/[,]/g, '')
+            d["price"] = +d["price"]
         });
-        
-        let maxprice = d3.max(price);
-        console.log(maxprice);
-        let minprice = d3.min(price);
-        console.log(minprice);
-        var x = d3.scaleLinear()
-                .domain([0, maxprice])
-                .range([50, 500]);
-            svg.append("g")
-                .attr("transform", "translate(0," + 500 + ")")
-                .call(d3.axisBottom(x));
 
 
-        var histogram = d3.histogram()
-                        .value(function(price) { return price; })
-                        .domain(x.domain())
-                        .thresholds(x.ticks(40));
+        makeHistogram(data)
+    })
 
-        var bins = histogram(price);
+}
 
-        var y = d3.scaleLinear()
-                .domain([d3.max(bins, function(d) { return d.length; }), 0])
-                .range([0,450]);
-            svg.append("g")
-                .call(d3.axisLeft(y));
+function makeHistogram(data) {
+    let specData = data.filter(data => data["date"] === "2020-02")
 
-        drawAxes(x,y);
+    specData = specData.filter(function(v) {
+        return this[v.listing_id]?
+          !Object.assign(this[v.listing_id], v):
+          (this[v.listing_id] = v);
+      }, {});
+    let price = specData.map(row => row["price"])
 
-        svg.selectAll("rect")
-            .data(bins)
-            .enter()
-            .append("rect")
-            .attr("x", 1)
-            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-            .attr("width", function(d) { return x(d.x1) - x(d.x0); })
-            .attr("height", function(d) { return measurements.height - y(d.length); })
-            .style("fill", "steelblue")
-            .on("mouseover", function (d) {
-                let tempData = data.filter(data => data["monthly_price"] >= d.x0 && data["monthly_price"] <= d.x1)
-                tooltip.transition()
+    let maxprice = d3.max(price);
+    let minprice = d3.min(price);
+    var x = d3.scaleLinear()
+        .domain([minprice, maxprice + 125])
+        .range([100, 550]);
+    svg.append("g")
+        .attr("id", "x-axis")
+        .attr("transform", "translate(0," + 500 + ")")
+        .call(d3.axisBottom(x))
+        .call(g => g.append("text") // Adds the x-axis label to the x-axis
+            .attr("class", "x label")
+            .attr("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("x", 700/2)
+            .attr("y", (40))
+            .attr("fill", "black")
+            .text("Avg. Monthly Price"));
+
+
+    var histogram = d3.histogram()
+        .value(function (price) { return price; })
+        .domain(x.domain())
+        .thresholds(x.ticks(40));
+
+    var bins = histogram(price);
+
+    var y = d3.scaleLinear()
+        .domain([d3.max(bins, function (d) { return d.length; }), 0])
+        .range([0, 450]);
+    svg.append("g")
+        .attr("id", "y-axis")
+        .attr('transform', 'translate(100,50)')
+        .call(d3.axisLeft(y))
+        .call(g => g.append("text")
+            .attr("text-anchor", "middle")
+            .attr("font-size", "12px")
+            .attr("x", -225)
+            .attr("y", -50)
+            .attr("fill", "black")
+            .attr("transform", "rotate(-90)")
+            .text("Number of Homes in bin"));
+
+    drawAxes(x, y);
+
+    svg.selectAll('.rect')
+        .data(bins)
+        .enter()
+        .append('rect')
+        .attr('x', function (d) { return x(d.x0 + 50) })
+        .attr('y', function (d) { return y(d.length) + 49 })
+        .attr('width', function (d) { return x(d.x1) - x(d.x0) })
+        .attr('height', function (d) { return 450 - y(d.length) })
+        .attr('fill', 'steelblue')
+        .attr("stroke", "black")
+        .on("mouseover", function (d) {
+            let tempData = specData.filter(data => data["price"] >= d.x0 && data["price"] <= d.x1)
+            tooltip.transition()
                 .duration(200)
                 .style("opacity", 1);
 
-                tooltip.style("left", (d3.event.pageX + 30) + "px")
-                .style("top", (d3.event.pageY - 120) + "px");
+            tooltip.style("left", (d3.event.pageX + 30) + "px")
+                .style("top", (d3.event.pageY - 180) + "px");
 
-                let tSvg = tooltip.append("svg")
-                    .attr("height", "300px")
-                    .attr("width", "325px")
+            let tSvg = tooltip.append("svg")
+                .attr("height", "300px")
+                .attr("width", "325px")
 
-                // Appends text containing the name of the country that was hovered over to the linegraph
-                tSvg.append("text")
-                    .attr("text-anchor", "middle")
-                    .attr("font-size", "16px")
-                    .attr("x", (325) / 2)
-                    .attr("y", (15))
-                    .attr("fill", "black")
-                    // .text(country);
+            tSvg.append("text")
+                .attr("text-anchor", "middle")
+                .attr("font-size", "16px")
+                .attr("x", (325) / 2)
+                .attr("y", (15))
+                .attr("fill", "black")
 
-                // nygeo data which contains information on the overall nyc map 
-                d3.json('data/neighbourhoods.geojson').then(function(mapdata) {
-                    // console.log(mapdata)
-                    // // data which contains information on individual points
-                    // d3.csv('listings.csv').then(function(pointData) {
+            d3.json('data/neighbourhoods.geojson').then(function (mapdata) {
 
-                        // scaling function to convert point data to map. Rotated to proper lat/long
-                        const albersProj = d3.geoAlbers()
-                            .scale(75000)
-                            .rotate([122.3321, 0])
-                            .center([0, 47.6062])
-                            .translate([325/2, 300/2]);
+                d3.csv('data/listings.csv').then(function (pointData) {
+                    let ids = tempData.map(row => row['listing_id'])
+                    let filtPoints = pointData.filter(
+                        function (e) {
+                            return this.indexOf(e.id) >= 0;
+                        },
+                        ids
+                    );
 
-                        // projects points onto the map
-                        const geoPath = d3.geoPath()
+                    const albersProj = d3.geoAlbers()
+                        .scale(65000)
+                        .rotate([122.3321, 0])
+                        .center([0, 47.6062])
+                        .translate([325 / 2, 300 / 2]);
+
+                    const geoPath = d3.geoPath()
                         .projection(albersProj)
 
-                        // grouping for paths which compose nyc map
-                        let sea = tSvg.append( "g" ).attr( "id", "sea" );
-                        sea.selectAll('path')
+                    let sea = tSvg.append("g").attr("id", "sea");
+                    sea.selectAll('path')
                         .data(mapdata.features)
                         .enter()
                         .append('path')
-                            .attr('fill', 'lightgray')
-                            .attr('stroke', 'black')
-                            .attr('d', geoPath)
+                        .attr('fill', 'lightgray')
+                        .attr('stroke', 'black')
+                        .attr('d', geoPath)
 
-                        // plots circles on the nyc map and adds on-click function 
-                        // that transitions point to new direction + removes point from map.
-                        let bnb = tSvg.append( "g" ).attr( "id", "bnb" );
-                        bnb.selectAll('.circle')
-                            .data(tempData)
-                            .enter()
-                            .append('circle')
-                                .attr('cx', function(d) { 
-                                    let scaledPoints = albersProj([d['longitude'], d['latitude']])
-                                    return scaledPoints[0]
-                                })
-                                .attr('cy', function(d) {
-                                    let scaledPoints = albersProj([d['longitude'], d['latitude']])
-                                    return scaledPoints[1]
-                                })
-                                .attr('r', 4)
-                                .attr('fill', 'steelblue')
-                                .attr("stroke", "black")
-                                .on( "click", function(){
-                                    let thing = d3.select(this)
-                                        .attr("opacity", 1)
-                                        .transition()
-                                            .duration( 2000 )
-                                            .attr( "cy", 1000 )
-                                            .attr( "opacity", 0 )
-                                            .on("end", function(thing) {
-                                                d3.select(this).remove();
-                                            })
-                                });
+                    let bnb = tSvg.append("g").attr("id", "bnb");
+                    bnb.selectAll('.circle')
+                        .data(filtPoints)
+                        .enter()
+                        .append('circle')
+                        .attr('cx', function (d) {
+                            let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                            return scaledPoints[0]
+                        })
+                        .attr('cy', function (d) {
+                            let scaledPoints = albersProj([d['longitude'], d['latitude']])
+                            return scaledPoints[1]
+                        })
+                        .attr('r', 4)
+                        .attr('fill', 'steelblue')
+                        .attr("stroke", "black")
 
-                        
-                    // })
-                
+
                 })
+
             })
-            .on("mouseout", function (d) {
-                tooltip.transition()
-                    .duration(0)
-                    .style("opacity", 0)
-                tooltip.selectAll("svg")
-                    .attr("height", "0px")
-                    .attr("width", "0px")
-            });
-        }
+        })
+        .on("mouseout", function (d) {
+            tooltip.transition()
+                .duration(0)
+                .style("opacity", 0)
+            tooltip.selectAll("svg")
+                .attr("height", "0px")
+                .attr("width", "0px")
+        });
 
-    function drawAxes(scaleX, scaleY) {
-        let xAxis = d3.axisBottom()
-            .scale(scaleX)
+    let months = ["2020-02", "2020-03", "2020-04", "2020-05", "2020-06", "2020-07", "2020-08", "2020-09", "2020-10", "2020-11", "2020-12", "2021-01", "2021-02"]
 
-        let yAxis = d3.axisLeft()
-            .scale(scaleY)
+    let monthFilter = d3.select("#filterMonth")
+        .append('select')
+    
+    monthFilter.selectAll('option')
+        .data(months)
+        .enter()
+        .append('option')
+        .attr('value', function (d) { return d })
+        .html(function (d) { return d })
 
-        svg.append('g')
-            .attr('transform', 'translate(50,50)')
-            .call(yAxis)
+    monthFilter.on("change", function () {
+        console.log("change")
+        let newData = data;
+        newData = newData.filter(data => data["date"] == this.value);
+        newData = newData.filter(function (v) {
+            return this[v.listing_id] ?
+                !Object.assign(this[v.listing_id], v) :
+                (this[v.listing_id] = v);
+        }, {});
+        let price = newData.map(row => row["price"])
+        let extent = d3.extent(price);
+        console.log(extent)
+        x.domain([extent[0], extent[1] + 125])
+        histogram = d3.histogram()
+            .value(function (price) { return price; })
+            .domain(x.domain())
+            .thresholds(x.ticks(40));
+        y.domain([d3.max(bins, function (d) { return d.length; }), 0])
 
+        drawAxes(x, y)
 
-        }
-})()
+        d3.select('#x-axis')
+            .call(d3.axisBottom(x));
+
+        d3.select('#y-axis')
+            .call(d3.axisLeft(y));
+        
+        let bar = svg.selectAll(".rect").data(data)
+        bar.exit().remove()
+
+        bar.transition()
+            .duration(1000)
+            .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+
+        bar.select("rect")
+            .transition()
+            .duration(1000)
+            .attr("height", function(d) { return height - y(d.y); })
+            .attr("fill", function(d) { return colorScale(d.y) });
+
+    })
+}
+
+function drawAxes(scaleX, scaleY) {
+    let xAxis = d3.axisBottom()
+        .scale(scaleX)
+
+    let yAxis = d3.axisLeft()
+        .scale(scaleY)
+
+}
